b102fe8090e9b03f76201fd7bf1d79ef73831711
Raden Ahmad Yasin Mahendra <yasinmahendra26@gmail.com>
2025-04-09 16:15:42 +0700

    Fixing ci.yml

--- src/test/java/id/ac/ui/cs/advprog/gatherlove/GatherloveApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/gatherlove/GatherloveApplicationTests.java
@@ -3,7 +3,7 @@ package id.ac.ui.cs.advprog.gatherlove;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.config.location=classpath:application.properties"})
 class GatherloveApplicationTests {
 
 	@Test
