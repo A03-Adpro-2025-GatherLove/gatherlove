e54ba94916b484c44b2698192ec88cac3b55a268
lantry-glitch <allan.kwek.18@gmail.com>
2025-04-11 01:17:10 +0700

    [RED] feat(donation): Add query method skeletons to DonationRepository

--- src/main/java/id/ac/ui/cs/advprog/gatherlove/donation/repository/DonationRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/gatherlove/donation/repository/DonationRepository.java
@@ -3,9 +3,12 @@ package id.ac.ui.cs.advprog.gatherlove.donation.repository;
 import id.ac.ui.cs.advprog.gatherlove.donation.model.Donation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.UUID;
 
 @Repository // Added
 public interface DonationRepository extends JpaRepository<Donation, UUID> {
-    // Basic CRUD inherited
+    List<Donation> findByDonorIdOrderByDonationTimestampDesc(UUID donorId);
+    List<Donation> findByCampaignIdOrderByDonationTimestampDesc(UUID campaignId);
 }
