16bcdafcdbda9cf5e975a141b5d5dc6de78b5eff
githubhiseka <susantoabhiseka@gmail.com>
2025-04-11 15:28:04 +0700

    fix(red): trying to resolve import

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security")
 
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    testImplementation("org.springframework.modulith:spring-modulith-starter-test:1.1.1")
+
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.postgresql:postgresql")
