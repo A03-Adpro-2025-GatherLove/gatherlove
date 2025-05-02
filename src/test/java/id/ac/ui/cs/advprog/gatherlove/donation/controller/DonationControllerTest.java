4097b432f4662107b594bc3480e7a8c75aa1a785
lantry-glitch <allan.kwek.18@gmail.com>
2025-05-02 17:33:59 +0700

    [REFACTOR] Replace deprecated @MockBean/@MockBeans with @MockitoBean

--- src/test/java/id/ac/ui/cs/advprog/gatherlove/donation/controller/DonationControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/gatherlove/donation/controller/DonationControllerTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.security.test.context.support.WithMockUser; // For testing security
@@ -38,7 +38,7 @@ class DonationControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private DonationService donationService;
 
     @Autowired
