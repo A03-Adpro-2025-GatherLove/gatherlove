ee09ee7ca923965e5ba8c029099caf346c3ac20b
githubhiseka <susantoabhiseka@gmail.com>
2025-05-07 08:11:42 +0700

    fix: changed typo from auto refactoring

--- src/main/java/id/ac/ui/cs/advprog/gatherlove/authentication/service/CustomUserDetailsService.java
+++ src/main/java/id/ac/ui/cs/advprog/gatherlove/authentication/service/CustomUserDetailsService.java
@@ -6,6 +6,7 @@ import id.ac.ui.cs.advprog.gatherlove.authentication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -28,18 +29,16 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity userEntity = userRepository.findByUsername(username)
+        UserEntity user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
 
-        return new UserEntity(
-                userEntity.getUsername(),
-                userEntity.getPassword(),
-                mapRolesToAuthorities(userEntity.getRoles()));
+        return new User(
+                user.getUsername(),
+                user.getPassword(),
+                mapRolesToAuthorities(user.getRoles()));
     }
 
-    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Set<Role> roles) {
-        return roles.stream()
-                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
-                .collect(Collectors.toList());
+    private Collection<GrantedAuthority> mapRolesToAuthorities(Set<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
 }
