8e7018ab71b16ac52d3516a7b3cadda49073a5f4
Raden Ahmad Yasin Mahendra <yasinmahendra26@gmail.com>
2025-05-10 15:21:01 +0700

    Change from Long to UUID

--- src/main/java/id/ac/ui/cs/advprog/gatherlove/admin/service/AdminDashboardService.java
+++ src/main/java/id/ac/ui/cs/advprog/gatherlove/admin/service/AdminDashboardService.java
@@ -1,8 +1,10 @@
 package id.ac.ui.cs.advprog.gatherlove.admin.service;
 
+import java.util.UUID;
+
 import id.ac.ui.cs.advprog.gatherlove.admin.dto.Stats;
 
 public interface AdminDashboardService {
     Stats getStats();
-    void deleteUser(Long user_id);
+    void deleteUser(UUID user_id);
 }
