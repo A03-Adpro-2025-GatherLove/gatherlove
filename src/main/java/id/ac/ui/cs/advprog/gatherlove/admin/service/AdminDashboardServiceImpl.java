80019bb36a30f8dfb5e5747753da871104d6959f
Raden Ahmad Yasin Mahendra <yasinmahendra26@gmail.com>
2025-05-10 13:45:27 +0700

    [GREEN] Implement AdminDashboardService

--- src/main/java/id/ac/ui/cs/advprog/gatherlove/admin/service/AdminDashboardServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/gatherlove/admin/service/AdminDashboardServiceImpl.java
@@ -1,13 +1,35 @@
 package id.ac.ui.cs.advprog.gatherlove.admin.service;
 
+import id.ac.ui.cs.advprog.gatherlove.campaign.repository.CampaignRepository;
+import id.ac.ui.cs.advprog.gatherlove.donation.repository.DonationRepository;
 import id.ac.ui.cs.advprog.gatherlove.admin.dto.Stats;
+import id.ac.ui.cs.advprog.gatherlove.authentication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminDashboardServiceImpl implements AdminDashboardService {
-
+    @Autowired
+    private CampaignRepository campaignRepository;
+    
+    @Autowired
+    private DonationRepository donationRepository;
+    
+    @Autowired
+    private UserRepository userRepository;
+    
     @Override
     public Stats getStats() {
-        return null;
+        Stats stats = new Stats();
+        
+        stats.setTotalCampaigns(campaignRepository.count());
+        
+        stats.setTotalDonations(donationRepository.count());
+        
+        stats.setTotalUsers(userRepository.count());
+        
+        stats.setTotalFundRaised(donationRepository.getTotalFundRaised());
+        
+        return stats;
     }
 }
