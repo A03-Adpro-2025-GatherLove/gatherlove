84d766f3b691f492a73d3276d526e2b2f3cb252b
Raden Ahmad Yasin Mahendra <yasinmahendra26@gmail.com>
2025-05-10 15:21:50 +0700

    Implement deleteUser

--- src/main/java/id/ac/ui/cs/advprog/gatherlove/admin/service/AdminDashboardServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/gatherlove/admin/service/AdminDashboardServiceImpl.java
@@ -3,7 +3,11 @@ package id.ac.ui.cs.advprog.gatherlove.admin.service;
 import id.ac.ui.cs.advprog.gatherlove.campaign.repository.CampaignRepository;
 import id.ac.ui.cs.advprog.gatherlove.donation.repository.DonationRepository;
 import id.ac.ui.cs.advprog.gatherlove.admin.dto.Stats;
+import id.ac.ui.cs.advprog.gatherlove.authentication.model.UserEntity;
 import id.ac.ui.cs.advprog.gatherlove.authentication.repository.UserRepository;
+
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,5 +35,11 @@ public class AdminDashboardServiceImpl implements AdminDashboardService {
         stats.setTotalFundRaised(donationRepository.getTotalFundRaised());
         
         return stats;
+    }
+
+    @Override
+    public void deleteUser(UUID id) {
+        UserEntity user = userRepository.findById(id).orElseThrow();
+        userRepository.delete(user);
     }
 }
