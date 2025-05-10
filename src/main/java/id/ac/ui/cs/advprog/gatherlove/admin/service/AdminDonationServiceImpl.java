3cd6806b4bf33b4143bcbb7ee3ae3344d08f7b2e
Raden Ahmad Yasin Mahendra <yasinmahendra26@gmail.com>
2025-05-10 11:47:20 +0700

    [RED] make tests for AdminDonationService

--- src/main/java/id/ac/ui/cs/advprog/gatherlove/admin/service/AdminDonationServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/gatherlove/admin/service/AdminDonationServiceImpl.java
@@ -1,20 +1,16 @@
 package id.ac.ui.cs.advprog.gatherlove.admin.service;
 
-import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.stereotype.Service;
 
-import id.ac.ui.cs.advprog.gatherlove.donation.model.DonationDummy;
+import id.ac.ui.cs.advprog.gatherlove.donation.model.Donation;
 
 @Service
 public class AdminDonationServiceImpl implements AdminDonationService {
+
     @Override
-    public List<DonationDummy> getDonationHistory() {
-        // dummy data
-        List<DonationDummy> donationHistory = new ArrayList<>();
-        donationHistory.add(new DonationDummy(1L, 100L, "John Doe"));
-        donationHistory.add(new DonationDummy(2L, 200L, "Jane Doe"));
-        donationHistory.add(new DonationDummy(3L, 300L, "Bob Smith"));
-        return donationHistory;
+    public List<Donation> getDonationHistory() {
+        return null;
     }
 }
