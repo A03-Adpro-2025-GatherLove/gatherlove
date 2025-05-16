0f60def397d30468267c9add4df0e1037220ddea
githubhiseka <susantoabhiseka@gmail.com>
2025-05-16 17:28:28 +0700

    fix: changed type to UUID

--- src/main/java/id/ac/ui/cs/advprog/gatherlove/profile/repository/ProfileRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/gatherlove/profile/repository/ProfileRepository.java
@@ -3,5 +3,7 @@ package id.ac.ui.cs.advprog.gatherlove.profile.repository;
 import id.ac.ui.cs.advprog.gatherlove.profile.model.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
+import java.util.UUID;
+
+public interface ProfileRepository extends JpaRepository<Profile, UUID> {
 }
