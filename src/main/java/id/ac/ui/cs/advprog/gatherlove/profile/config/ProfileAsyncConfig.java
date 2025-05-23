3df139c6c952b3c29184a6dedbe816c9723db630
githubhiseka <susantoabhiseka@gmail.com>
2025-05-23 20:22:23 +0700

    fix: renamed profile async class
    done to prevent error with other async config classes inside the project

--- src/main/java/id/ac/ui/cs/advprog/gatherlove/profile/config/ProfileAsyncConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/gatherlove/profile/config/ProfileAsyncConfig.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
-public class AsyncConfig {
+public class ProfileAsyncConfig {
     @Bean
     public TaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
