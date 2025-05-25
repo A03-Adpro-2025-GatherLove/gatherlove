f15aab86cfb75314a5f698b7f2bbea3c96329836
JanssenBenedict <janssen.benedict@gmail.com>
2025-05-25 10:45:59 +0700

    Implemented Paging System for Transactions Page

--- src/main/java/id/ac/ui/cs/advprog/gatherlove/wallet/dto/TransactionDto.java
+++ src/main/java/id/ac/ui/cs/advprog/gatherlove/wallet/dto/TransactionDto.java
@@ -1,12 +1,28 @@
 package id.ac.ui.cs.advprog.gatherlove.wallet.dto;
 
 import id.ac.ui.cs.advprog.gatherlove.wallet.enums.TransactionType;
+import lombok.Getter;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record TransactionDto(
-        Long transaction_id,
-        TransactionType type,
-        BigDecimal amount,
-        LocalDateTime timestamp
-) {}
+@Getter
+public class TransactionDto {
+    private final Long transaction_id;
+    private final String type;
+    private final BigDecimal amount;
+    private final LocalDateTime timestamp;
+    private final String displayType;
+
+    public TransactionDto(Long id, TransactionType type, BigDecimal amount, LocalDateTime timestamp) {
+        this.transaction_id = id;
+        this.type = type.name();
+        this.displayType = switch (type) {
+            case TOP_UP    -> "Top-Up to Wallet";
+            case WITHDRAW  -> "Withdrawal from Campaign";
+            case DONATION  -> "Donation to Campaign";
+        };
+        this.amount = amount;
+        this.timestamp = timestamp;
+    }
+}
