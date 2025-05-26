6c5fa633e89455d5910021ca3b7005e96c57f88f
githubhiseka <susantoabhiseka@gmail.com>
2025-05-27 00:52:46 +0700

    feat: added setters for dto's

--- src/main/java/id/ac/ui/cs/advprog/gatherlove/profile/dto/ProfileRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/gatherlove/profile/dto/ProfileRequest.java
@@ -1,12 +1,10 @@
 package id.ac.ui.cs.advprog.gatherlove.profile.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/id/ac/ui/cs/advprog/gatherlove/profile/dto/ProfileResponse.java
+++ src/main/java/id/ac/ui/cs/advprog/gatherlove/profile/dto/ProfileResponse.java
@@ -6,6 +6,7 @@ import lombok.*;
 import java.util.UUID;
 
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
